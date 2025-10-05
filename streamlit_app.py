8d98a4a73c833a101844d3f518ee5ff1eb0c8b1d
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-05 21:39:43 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,10 @@ with st.sidebar:
     st.header("Immobilien Informationen")
 
     we = st.slider("Anzahl Wohneinheiten", min_value=1, max_value=25, value=2, step=1)
-    number = st.number_input("Jahresverbauch Wohnungen", min_value=1500, max_value=100000, value=2500, step=100)
+    verbrauch_WE = st.number_input("Jahresverbauch Wohnungen", min_value=1500, max_value=100000, value=2500, step=100)
     has_GE = st.toggle("Gewerbeeinheiten vorhanden?", value=False)
     pv = st.slider("PV-Anlage", min_value=1, max_value=100, value=10, step=1)
-    sp = st.slider("Speichergröße", min_value=0, max_value=100, value=0, sept=1)
+    speicher = st.slider("Speichergröße", min_value=0, max_value=100, value=0, step=1)
     has_WP = st.toggle("Wärmepumpe vorhanden?", value=False)
     
 # Wenn Gewerbeeinheiten vorhanden 
