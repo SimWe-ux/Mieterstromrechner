9ed2b528a58d96959908a97678dd10922dcff719
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-05 21:15:09 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,5 +10,4 @@ with st.sidebar:
     st.header("Eingabe")
 
     we = st.slider("Anzahl Wohneinheiten", min_value=1, max_value=25, value=2, step=1)
-
- number = st.number_input("Gesamtverbrauch", min_value=1500, max_value=100000, value=2500)
+    number = st.number_input("Gesamtverbrauch", min_value=1500, max_value=100000, value=2500)
