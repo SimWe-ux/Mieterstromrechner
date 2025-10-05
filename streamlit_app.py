b053892ea35d1046623a3dc5bc530aa1024b1c26
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-05 21:32:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,10 @@ with st.sidebar:
     st.header("Immobilien Informationen")
 
     we = st.slider("Anzahl Wohneinheiten", min_value=1, max_value=25, value=2, step=1)
-    number = st.number_input("Gesamtverbrauch", min_value=1500, max_value=100000, value=2500, step=100)
+    number = st.number_input("Jahresverbauch Wohnungen", min_value=1500, max_value=100000, value=2500, step=100)
     has_GE = st.toggle("Gewerbeeinheiten vorhanden?", value=False)
 # Wenn Gewerbeeinheiten vorhanden 
     if has_GE: #
         st.markdown("---")
         st.markdown("**Gewerbeeinheiten**") 
-        has_GE = st.number_input("Verbrauch Gewerbeeinheiten", min_value=2500, max_value=100000, value=2500, step=100)    
+        has_GE = st.number_input("Jahresverbrauch Gewerbeeinheiten", min_value=2500, max_value=100000, value=2500, step=100)    
