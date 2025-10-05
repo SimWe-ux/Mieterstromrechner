73e0454ce0f12804039a0cc391a5d55799052782
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-05 14:35:18 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,20 +4,35 @@ import streamlit as st
 st.set_page_config(page_title="Mieterstrom Rechner", page_icon="⚡", layout="centered")
 st.title("⚡ Mieterstrom - Renditerechner")
 
-# ----Konstante----
-PV_Form_Exponent = 3 # PV Tageskurve 
-Ladeleistung_kW = 3 # Max. Ladeleistung (kW) – konservativ
-Entladeleistung_kW = 3 # Max. Endladeleistung (kW) – konservativ
-Wirkungsgrad_Roundtrip = 0.85 # Gesamtwirkungsgrad Laden*Entladen
-Standby_Watt = 20 # Optionale Standby‑/Eigenverbrauchsverluste des Speichers (W)
-SOC_Start_kWh = "Speichergröße"*20% # Start‑Ladezustand (kWh)  
-PV-Anlage & Speicher = if("PV_Anlage"<10, "PV_Anlage"*1500, if(and("PV_Anlage">=10, "PV_Anlage"<=20), "PV_Anlage"*1100, if("PV_Anlage">=20, "PV_Anlage"*990)))
-Speicherksoten  = 500 # Speicherkosten in € 
-Reststromksoten = 0.35 # Reststromkosten in € 
-PV-Stromksoten = 0.27 # PVstromksoten in € 
-Grundgebühren = 10 # Grundgebühren in € 
-Mieterstromzuschlage = 0.0238 # EEG Mieterstromzuschlag in € 
-Einspeisevergütung = 0.0688 # Einspeisevergütung in €
-Abrechnungskosten = 70 # Abbrechnungssoftwarekosten 
-Zählergebühren WE = 30 # POG Zählergebühren 
-Zählergebühren PV = 50 # POG Zählergebühren PV/WP Zähler 
+# ----Pv und Battrerie----
+pv_form_exponent = 3 # PV Tageskurve 
+ladeleistung = 3 # Max. Ladeleistung (kW) – konservativ
+entladeleistung = 3 # Max. Endladeleistung (kW) – konservativ
+wirkungsgrad_roundtrip = 0.85 # Gesamtwirkungsgrad Laden*Entladen
+standby_watt = 20 # Optionale Standby‑/Eigenverbrauchsverluste des Speichers (W)
+soc_start_kwh = "Speichergröße"*20% # Start‑Ladezustand (kWh)  
+
+# ----Preise und Vergütung----
+preis_pv_u10_kwp = 1500.0
+preis_pv_10_20_kwp = 1100.0
+preis_pv_o20_kwp = 990.0
+speicherksoten  = 500 # Speicherkosten in € 
+reststromksoten = 0.35 # Reststromkosten in € 
+pv_stromksoten = 0.27 # PVstromksoten in € 
+grundgebuehren = 10 # Grundgebühren in € 
+mieterstromzuschlage = 0.0238 # EEG Mieterstromzuschlag in € 
+einspeiseverguetung = 0.0688 # Einspeisevergütung in €
+strompreissteigerung_pa: float = 0.03 # Strompreissteigerung pro Jahr 
+
+# ----Betriebskosten---- 
+abrechnungskosten = 70 # Abbrechnungssoftwarekosten 
+zaehlergebuehren_we = 30 # POG Zählergebühren 
+zaehlergebuehren_pv = 50 # POG Zählergebühren PV/WP Zähler 
+
+def pv_preis_pro_kwp(pv_kwp: float, c: Konstanten) -> float:
+    if pv_kwp < 10:
+        return c.preis_pv_u10_eur_kwp
+    elif pv_kwp <= 20:
+        return c.preis_pv_10_20_eur_kwp
+    else:
+        return c.preis_pv_o20_eur_kwp
