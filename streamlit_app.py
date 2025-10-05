2a76ed74045ce6c2b488e2039ed6e77c0e27c65c
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-05 20:44:31 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 # ----Seiteneinstellungen----
 st.set_page_config(page_title="Mieterstrom Rechner", page_icon="⚡", layout="centered")
-st.title("⚡ Mieterstrom - RenditeRechner - MRR")
+st.title("⚡ Mieterstrom - RenditeRechner")
 
 # ---- UI: Eingabe----
