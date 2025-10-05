38ef9a4359a383b7b7436860ce68725f934819c9
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-05 14:01:30 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,16 +8,16 @@ st.title("⚡ Mieterstrom - Renditerechner")
 PV_Form_Exponent = 3 # PV Tageskurve 
 Ladeleistung_kW = 3 # Max. Ladeleistung (kW) – konservativ
 Entladeleistung_kW = 3 # Max. Endladeleistung (kW) – konservativ
-Wirkungsgrad_Roundtrip = 0,85 # Gesamtwirkungsgrad Laden*Entladen
+Wirkungsgrad_Roundtrip = 0.85 # Gesamtwirkungsgrad Laden*Entladen
 Standby_Watt = 20 # Optionale Standby‑/Eigenverbrauchsverluste des Speichers (W)
 SOC_Start_kWh = "Speichergröße"*20% # Start‑Ladezustand (kWh)  
 PV-Anlage & Speicher = if("PV_Anlage"<10, "PV_Anlage"*1500, if(and("PV_Anlage">=10, "PV_Anlage"<=20), "PV_Anlage"*1100, if("PV_Anlage">=20, "PV_Anlage"*990)))
 Speicherksoten  = 500 # Speicherkosten in € 
-Reststromksoten = 0,35 # Reststromkosten in € 
-PV-Stromksoten = 0,27 # PVstromksoten in € 
+Reststromksoten = 0.35 # Reststromkosten in € 
+PV-Stromksoten = 0.27 # PVstromksoten in € 
 Grundgebühren = 10 # Grundgebühren in € 
-Mieterstromzuschlage = 0,0238 # EEG Mieterstromzuschlag in € 
-Einspeisevergütung = 0,0688 # Einspeisevergütung in €
+Mieterstromzuschlage = 0.0238 # EEG Mieterstromzuschlag in € 
+Einspeisevergütung = 0.0688 # Einspeisevergütung in €
 Abrechnungskosten = 70 # Abbrechnungssoftwarekosten 
 Zählergebühren WE = 30 # POG Zählergebühren 
 Zählergebühren PV = 50 # POG Zählergebühren PV/WP Zähler 
