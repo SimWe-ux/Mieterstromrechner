876ca3d8a53ff92f2d6e1729dcd9aef1c8a5ccf3
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-06 22:49:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,26 +11,27 @@ st.title("⚡ Mieterstrom - RenditeRechner")
 with st.sidebar:
     st.header("Immobilien Informationen")
     
-# Wohneinheiten & Jahresverbrauch 
+    # Wohneinheiten & Jahresverbrauch 
     we = st.slider("Anzahl Wohneinheiten", min_value=1, max_value=25, value=2, step=1)
-    verbrauch_we = st.number_input("Jahresverbauch Wohnungen", min_value=1500, max_value=100000, value=2500, step=100)
+    we_verbrauch = st.number_input("Jahresverbrauch Wohnungen", min_value=1500, max_value=100000, value=2500, step=100)
 
-# Wenn Gewerbeeinheiten vorhanden 
-    has_GE = st.toggle("Gewerbeeinheiten vorhanden?", value=False)
+    # Wenn Gewerbeeinheiten vorhanden 
+    has_ge = st.toggle("Gewerbeeinheiten vorhanden?", value=False)
     if has_ge: #
-        has_ge = st.number_input("Jahresverbrauch Gewerbeeinheiten", min_value=2500, max_value=100000, value=2500, step=100)  
-   # PV Anlage ] Speicher 
+        ge_verbrauch = st.number_input("Jahresverbrauch Gewerbeeinheiten", min_value=2500, max_value=100000, value=2500, step=100)  
+   
+    # PV Anlage ] Speicher 
     pv = st.slider("PV-Anlage", min_value=1, max_value=100, value=10, step=1)
     speicher = st.slider("Speichergröße", min_value=0, max_value=100, value=0, step=1)
 
-# Wenn Wärmepumpe vorhanden 
+    # Wenn Wärmepumpe vorhanden 
     has_wp = st.toggle("Wärmepumpe vorhanden?", value=False)   
     if has_wp: #
-        verbrauch_wp = st.number_input("Wärmepumpenverbrauch", min_value=1000, max_value=100000, value=2500, step=100)  
+        wp_verbrauch = st.number_input("Wärmepumpenverbrauch", min_value=1000, max_value=100000, value=2500, step=100)  
 
 # ----Mapping in Configurations.py---
 C.wohneinheiten = int(we)
-C.wohnungen_verbrauch_kwh = float(verbrauch_we)
+C.wohnungen_verbrauch_kwh = float(we_verbrauch)
 C.gewerbe_aktiv = bool(has_ge)
 C.gewerbe_verbrauch_kwh = float(ge_verbrauch) if has_ge else 0.0
 C.pv_kwp = float(pv)
