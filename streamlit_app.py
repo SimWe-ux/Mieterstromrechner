f07deeddf233d029a3429f04ff3ddf0af7753f7c
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-12 14:26:29 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -272,6 +272,15 @@ df_long = df_plot.reset_index(drop=True).melt(
     var_name="Serie",
     value_name="kWh"
 )
+horizon_map = {
+    "1 Months": "1mo",
+    "3 Months": "3mo",
+    "6 Months": "6mo",
+    "1 Year": "1y",
+    "5 Years": "5y",
+    "10 Years": "10y",
+    "20 Years": "20y",
+}
 
 st.subheader("Monatswerte – Jahresverlauf")
 st.line_chart(df_m)
