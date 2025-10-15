cb5e655f2c884fc07104e835250fff89608d1a18
Simon Wedeking <simon.wedeking@gmx.de>
2025-10-15 18:16:03 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,11 +45,16 @@ with st.sidebar:
     )
 
 
+    # Wohneinheiten & Jahresverbrauch 
+    we = st.slider("Anzahl Wohneinheiten", min_value=0, max_value=25, value=2, step=1)
+    we_verbrauch = st.number_input("Jahresverbrauch Wohnungen (kWh)", min_value=1000, max_value=100000, value=2500, step=100)
+    
     # Wenn Gewerbeeinheiten vorhanden 
     has_ge = st.toggle("Gewerbeeinheiten vorhanden?", value=False)
     if has_ge: #
         ge_verbrauch = st.number_input("Jahresverbrauch Gewerbeeinheiten (kWh)", min_value=2500, max_value=100000, value=2500, step=100)  
-   
+
+    
     # PV Anlage ] Speicher 
     pv = st.slider("PV-Anlage (kWp)", min_value=1, max_value=100, value=10, step=1)
     speicher = st.slider("Speichergröße (kWh)", min_value=0, max_value=100, value=0, step=1)
